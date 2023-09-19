e72ff83ae74e267646ced6bc7e635a2ddccbb7cb
Suraj Kumar <surajk@capitalnumbers.com>
2023-09-19 12:31:22 +0530

    expense filter component added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expense = ({ expenses }) => {
+
+    const [ selectedValue, setSelectedValue ] = useState('2021')
+
+    const filterSelectHandler = (fiterValue) => {
+        setSelectedValue(fiterValue)
+    }
+
     return (
         <Card className='expenses'>
+            <div>
+                <ExpensesFilter selectedValue={selectedValue} onChangeYear={ filterSelectHandler } />
+            </div>
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
