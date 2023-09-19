baa6403825617256af4bd982eeddcb7fecb005b1
Suraj Kumar <surajk@capitalnumbers.com>
2023-09-19 12:10:53 +0530

    update in expense list ui and components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,9 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 function ExpenseItem({ date, title, amount }) {
     return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
