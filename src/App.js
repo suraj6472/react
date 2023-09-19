046bc2ca8157d4ca405d00c438a1d10257f2c494
Suraj Kumar <surajk@capitalnumbers.com>
2023-09-19 12:18:54 +0530

    react imported in all js files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expense from "./components/Expenses";
 function App() {
   const expenses = [
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './ExpenseDate.css'
 function ExpenseDate({ date }) {
     const month = date.toLocaleString('en-US', {month: 'long'})
