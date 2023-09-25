ab069be06d08575ac884fd06ad115ef30b85b883
Suraj Kumar <surajk@capitalnumbers.com>
2023-09-25 20:24:44 +0530

    email state handling via reducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,45 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+
+  if (action.type == 'USER_INPUT') {
+    return {
+      value: action.val,
+      isValid: action.val.includes('@')
+    }
+  }
+  if (action.type == 'BLUR_INPUT') {
+    return {
+      value: state.value,
+      isValid: state.value.includes('@')
+    }
+  }
+  return {
+    value: '',
+    isValid: false
+  }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null
+  });
 
-    // we doing this settimeout implementation to avoid input check for all key strokes
-    // instead we check input once user pause while entering input
-    const identifier = setTimeout(() => {
-      console.log('validating')
-      setFormIsValid(
-        enteredPassword.trim().length > 6 && enteredEmail.includes('@')
-      );
-    }, 500)
+  // useEffect(() => {
 
-    //NOTE: this return ClEAN UP function executes before executing the settimeout function 
-    return () => {
-      console.log("CLEAN UP")
-      clearTimeout(identifier)
-    }
+  //   // we doing this settimeout implementation to avoid input check for all key strokes
+  //   // instead we check input once user pause while entering input
+  //   const identifier = setTimeout(() => {
+  //     console.log('validating')
+  //     setFormIsValid(
+  //       enteredPassword.trim().length > 6 && enteredEmail.includes('@')
+  //     );
+  //   }, 500)
 
-  }, [enteredEmail, enteredPassword]);
+  //   //NOTE: this return ClEAN UP function executes before executing the settimeout function 
+  //   return () => {
+  //     console.log("CLEAN UP")
+  //     clearTimeout(identifier)
+  //   }
+
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    // setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(emailState.value.includes('@'));
+    dispatchEmail({ type: 'BLUR_INPUT' })
   };
 
   const validatePasswordHandler = () => {
@@ -48,21 +81,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
