a9fe35dd75772c6580c72259b3376c56eae391cf
Suraj Kumar <surajk@capitalnumbers.com>
2023-10-09 10:24:49 +0530

    component reevaluation comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  // component always re-evaluate when props, state and context changes
+  // re-evaluation does not mean rerendering the component
+  // react does whole communication between the component. it is react-dom's 
+  // work to bring some change on the screen by comparing virtual dom with actual dom
+  
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
