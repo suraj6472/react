1ee39e7d0c39c2a8ae19dbda8a27336281d6e52d
Suraj Kumar <surajk@capitalnumbers.com>
2023-10-09 10:41:39 +0530

    example of dependency array element when using useCallback

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
-  // sometime using React.memo is not enough to avoid unnecessary re-evaluation of component
-  // if a function is passed as props to tha component. this is because in js
-  // functions are object which is a reference value which is not equal to itself
-  // so each re-evaluation its value changes making the child component to re-evaluate
-  // to avoid this issue, we use "useCallback". it saves the function in memory and avoid assign in new values on parent's re-evaluation
+
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []); // this second parameter is used to set the dependency parameter for this function so that it could update the function when dependency changes
- 
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]); // this function updates when the dependency "allowToggle" changes
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
