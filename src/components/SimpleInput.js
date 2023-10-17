ce092f17716c8bd662f2f9c1235e338d592747b0
Suraj Kumar <surajk@capitalnumbers.com>
2023-10-17 10:52:06 +0530

    Handling the "was touched" State

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('')
-  const [isEnteredNameValid, setIsEnteredNameValid] = useState(true)
+  const [isEnteredNameValid, setIsEnteredNameValid] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
 
   const inputNameRef = useRef();
 
@@ -10,29 +11,37 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value)
   }
 
+  useEffect(() => {
+    if(isEnteredNameValid) {
+      console.log(isEnteredNameValid)
+    }
+  }, [isEnteredNameValid])
+
   const onSubmitHandler = event => {
     event.preventDefault();
 
+    setEnteredNameTouched(true)
     if (enteredName.trim() === '') {
       setIsEnteredNameValid(false)
       return;
     }
     setIsEnteredNameValid(true)
-
     console.log(enteredName)
 
     const enteredNameValue = inputNameRef.current.value
     console.log(enteredNameValue)
   }
 
-  const nameInputClasses = isEnteredNameValid ? 'form-control' : 'form-control invalid'
+  const nameInputFieldIsInValid = !isEnteredNameValid && enteredNameTouched
+
+  const nameInputClasses = nameInputFieldIsInValid ? 'form-control invalid' : 'form-control'
 
   return (
     <form onClick={onSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={onInputHandler} ref={inputNameRef} />
-        {!isEnteredNameValid && <p className='error-text'>Name field should not be empty</p>}
+        {nameInputFieldIsInValid && <p className='error-text'>Name field should not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
