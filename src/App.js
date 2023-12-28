b498f880f372bcbd421a56e28f1cfc92e24e0e84
Suraj Kumar <suraj6472@gmail.com>
2023-12-28 18:22:27 +0530

    additional routes in list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductPage /> }
 ])
 
 function App() {
---
+++ src/pages/Product.js
@@ -0,0 +1,5 @@
+function ProductPage() {
+    return <h1>This is products page</h1>
+}
+
+export default ProductPage
